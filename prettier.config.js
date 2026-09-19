75fc44c894b0ba017a7d97ab68bad5429cfb8ce2
Vytas <klimaviciusvytas@gmail.com>
2026-09-19 20:34:03 +0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+}
+
+export default config
