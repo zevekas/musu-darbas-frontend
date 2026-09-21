f287cbd927d752036f630bc81fb117f4f295eb36
zevekas <90040867+zevekas@users.noreply.github.com>
2026-09-21 20:22:29 +0300

    feat(MD-10): add auth (#2)
    * feat(MD-10): add auth
    
    * refactor(MD-10): change text

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ function Home() {
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
       <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
+        Edit <code>src/routes/index.tsx</code> to get started. Laizyk pyzda.
       </p>
     </div>
   )
